e4b49ac215399a6d7fb84a194914533f543c2a4f
Simon Mo <xmo@berkeley.edu>
2019-11-11 10:07:07 -0800

    Update assets again

---
+++ static/precache-manifest.f78e1d96d6332817e92a4ce102125ac0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d1d4e73154c8ce21868a5921b6f15c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b8a3d13ce918fffe583",
+    "url": "/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "4f50f3861c518476db4c",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "0b8a3d13ce918fffe583",
+    "url": "/static/js/2.d75fe505.chunk.js"
+  },
+  {
+    "revision": "4f50f3861c518476db4c",
+    "url": "/static/js/main.2828cda4.chunk.js"
+  },
+  {
+    "revision": "ca98f219beecd20a7ea2",
+    "url": "/static/js/runtime~main.da7fbd6b.js"
+  },
+  {
+    "revision": "b4e784d725242c64717bf21d01c540fd",
+    "url": "/static/media/linux.b4e784d7.svg"
+  },
+  {
+    "revision": "e0120624e85edb4cd44d6a59ebf4d375",
+    "url": "/static/media/python.e0120624.svg"
+  }
+]);
